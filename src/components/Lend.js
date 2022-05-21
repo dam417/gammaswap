448b205f24c4126303d7593ca5e09a533549b6b8
Pochetes <robertomiguel2001@gmail.com>
2022-05-21 05:06:08 -0400

    cleaned up syntax and code structure

--- src/components/Lend.js
+++ src/components/Lend.js
@@ -1,30 +1,15 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
 import {
     FormControl,
     FormLabel,
     Input,
     Button,
     Heading,
-    FormErrorMessage,
-    FormHelperText,
 } from '@chakra-ui/react';
 import { constants } from 'ethers';
 
 function Lend({ account, depPool, token0, token1}) {
-    // Declare a new state variable, which we'll call "count"
-    /*const [tokenA, setTokenA] = useState(0);
-     const [tokenB, setTokenB] = useState(0);
-
-     let navigate = useNavigate();
-
-     const routeChange = (id) =>{
-     console.log("routeChange >> " + id);
-     let path = `/pool/${id}`;
-     navigate(path);
-     }/**/
-
     const [token0Amt, setToken0Amt] = useState(0);
     const [token1Amt, setToken1Amt] = useState(0);
     const [liquidityAmt, setLiquidityAmt] = useState(0);
@@ -51,36 +36,36 @@ function Lend({ account, depPool, token0, token1}) {
         /*Lend LP Form */
         <>
             <form onSubmit={handleSubmit(deposit)}>
-            <Heading>Lend</Heading>
-            <FormControl boxShadow='lg'>
-                <Heading size='md'>Balance</Heading>
-                <FormLabel htmlFor='token0'>{token0.symbol}</FormLabel>
-                <Input
-                    placeholder='amount'
-                    id='token0'
-                    type='text'
-                    {...register('token0')}
-                />
-                <FormLabel
-                    mt={5}
-                    htmlFor='token1'
-                >
-                    {token1.symbol}
-                </FormLabel>
-                <Input
-                    placeholder='amount'
-                    id='token1'
-                    type='text'
-                    {...register('token1')}
-                />
-                <Button
-                    my={5}
-                    colorScheme='blue'
-                    type='submit'
-                >
-                    Submit
-                </Button>
-            </FormControl>
+                <Heading>Lend</Heading>
+                <FormControl boxShadow='lg'>
+                    <Heading size='md'>Balance</Heading>
+                    <FormLabel htmlFor='token0'>{token0.symbol}</FormLabel>
+                    <Input
+                        placeholder='amount'
+                        id='token0'
+                        type='text'
+                        {...register('token0')}
+                    />
+                    <FormLabel
+                        mt={5}
+                        htmlFor='token1'
+                    >
+                        {token1.symbol}
+                    </FormLabel>
+                    <Input
+                        placeholder='amount'
+                        id='token1'
+                        type='text'
+                        {...register('token1')}
+                    />
+                    <Button
+                        my={5}
+                        colorScheme='blue'
+                        type='submit'
+                    >
+                        Submit
+                    </Button>
+                </FormControl>
             </form>
             <form onSubmit={handleSubmit2(withdraw)}>
                 <FormControl boxShadow='lg' mt={10}>
--- src/components/TabGroup.js
+++ src/components/TabGroup.js
@@ -1,34 +1,9 @@
-/**
- * Created by danielalcarraz on 5/20/22.
- */
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import * as React from 'react';
 import Borrow from './Borrow';
 import Lend from './Lend';
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 
-// const Tab = styled.button`
-//   font-size: 20px;
-//   padding: 10px 60px;
-//   cursor: pointer;
-//   opacity: 0.6;
-//   background: white;
-//   border: 0;
-//   outline: 0;
-//   ${({ active }) =>
-// active &&
-// `
-//     border-bottom: 2px solid black;
-//     opacity: 1;
-//   `}
-// `;
-// const ButtonGroup = styled.div`
-//   display: flex;
-// `;
-// const types = ['Lend', 'Borrow'];
-
 function TabGroup(props) {
-    // const [active, setActive] = useState(types[0]);
     return (
         <Tabs variant='soft-rounded' colorScheme='green'>
             <TabList>
@@ -37,30 +12,23 @@ function TabGroup(props) {
             </TabList>
             <TabPanels>
                 <TabPanel>
-                    <Lend depPool={props.depPool} account={props.account} token0={props.token0} token1={props.token1} />
+                    <Lend
+                        depPool={props.depPool}
+                        account={props.account}
+                        token0={props.token0}
+                        token1={props.token1} 
+                    />
                 </TabPanel>
                 <TabPanel>
-                    <Borrow posManager={props.posManager} account={props.account} token0={props.token0} token1={props.token1}/>
+                    <Borrow
+                        posManager={props.posManager}
+                        account={props.account}
+                        token0={props.token0}
+                        token1={props.token1}
+                    />
                 </TabPanel>
             </TabPanels>
         </Tabs>
-        // <>
-        // <ButtonGroup>
-        //     {types.map(type => (
-        //         <Tab
-        //             key={type}
-        //             active={active === type}
-        //             onClick={() => setActive(type)}
-        //         >
-        //             {type}
-        //         </Tab>
-        //     ))}
-        // </ButtonGroup>
-        // <p />
-        // {active == 'Lend' ? <Lend/> : <Borrow/>}
-        // </>
-    );
+    )
 }
 export default TabGroup
-// Usage
-//<TabGroup/>
