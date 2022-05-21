15c5423e0cc5a099d1ebea82e9c47ce0dc79f0f4
Edwar <edwar.gleon@gmail.com>
2022-05-20 22:59:33 -0400

    lend component

--- src/components/Lend.js
+++ src/components/Lend.js
@@ -3,6 +3,15 @@
  */
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    Button,
+    Heading,
+    FormErrorMessage,
+    FormHelperText,
+} from '@chakra-ui/react'
 
 function Lend(props) {
     // Declare a new state variable, which we'll call "count"
@@ -18,8 +27,36 @@ function Lend(props) {
      }/**/
 
     return (
+
+        /*Lend LP Form */
         <div>
-            Lend LPs Form
+          <Heading> Lend </Heading>  
+
+
+
+<>
+            <FormControl boxShadow='lg'>
+                <Heading size='md'>Balance</Heading>
+                <FormLabel htmlFor='token-0'>Token 0</FormLabel>
+                <Input placeholder='amount' id='token-0' type='text' />
+                <FormLabel mt={5} htmlFor='token-1'>Token 1</FormLabel>
+                <Input placeholder='amount' id='token-1' type='text' />
+                <Button my={5} colorScheme='blue'>Submit</Button>
+            </FormControl>
+
+            <FormControl boxShadow='lg' mt={10}>
+                <Heading size='md'>Withdraw</Heading>
+                <FormLabel mt={5} htmlFor='withdraw'></FormLabel>
+                    <Input id='withdraw' type='text' />
+                <Button my={5} colorScheme='blue'>Submit</Button>
+            </FormControl>
+</>
+
+
+
+
+
+            
         </div>
     )
 
