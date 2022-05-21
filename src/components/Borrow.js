fe49c649f07c13f13f6cf7f8fe47fc2045cb0479
Pochetes <robertomiguel2001@gmail.com>
2022-05-20 21:18:49 -0400

    implemented Borrow Tab UI

--- src/components/Borrow.js
+++ src/components/Borrow.js
@@ -1,10 +1,16 @@
-/**
- * Created by danielalcarraz on 5/20/22.
- */
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    Button,
+    Heading,
+    FormErrorMessage,
+    FormHelperText,
+} from '@chakra-ui/react'
 
-function Borrow(props) {
+function Borrow() {
     // Declare a new state variable, which we'll call "count"
     /*const [tokenA, setTokenA] = useState(0);
      const [tokenB, setTokenB] = useState(0);
@@ -18,9 +24,24 @@ function Borrow(props) {
      }/**/
 
     return (
-        <div>
-            Borrow LPs Form
-        </div>
+        <>
+            <FormControl boxShadow='lg'>
+                <Heading size='md'>Open</Heading>
+                <FormLabel htmlFor='token-0'>Token 0</FormLabel>
+                <Input placeholder='amount' id='token-0' type='text' />
+                <FormLabel mt={5} htmlFor='token-1'>Token 1</FormLabel>
+                <Input placeholder='amount' id='token-1' type='text' />
+                <FormLabel mt={5} htmlFor='liquidity'>Liquidity</FormLabel>
+                <Input placeholder='amount' id='liquidity' type='text' />
+                <Button my={5} colorScheme='blue'>Submit</Button>
+            </FormControl>
+            <FormControl boxShadow='lg' mt={10}>
+                <Heading size='md'>Repay</Heading>
+                <FormLabel mt={5} htmlFor='liquidity'>Liquidity</FormLabel>
+                    <Input id='liquidity' type='text' />
+                <Button my={5} colorScheme='blue'>Submit</Button>
+            </FormControl>
+        </>
     );
 
 }
--- src/components/TabGroup.js
+++ src/components/TabGroup.js
@@ -5,44 +5,60 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Borrow from './Borrow';
 import Lend from './Lend';
-const Tab = styled.button`
-  font-size: 20px;
-  padding: 10px 60px;
-  cursor: pointer;
-  opacity: 0.6;
-  background: white;
-  border: 0;
-  outline: 0;
-  ${({ active }) =>
-active &&
-`
-    border-bottom: 2px solid black;
-    opacity: 1;
-  `}
-`;
-const ButtonGroup = styled.div`
-  display: flex;
-`;
-const types = ['Lend', 'Borrow'];
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+
+// const Tab = styled.button`
+//   font-size: 20px;
+//   padding: 10px 60px;
+//   cursor: pointer;
+//   opacity: 0.6;
+//   background: white;
+//   border: 0;
+//   outline: 0;
+//   ${({ active }) =>
+// active &&
+// `
+//     border-bottom: 2px solid black;
+//     opacity: 1;
+//   `}
+// `;
+// const ButtonGroup = styled.div`
+//   display: flex;
+// `;
+// const types = ['Lend', 'Borrow'];
 
 function TabGroup() {
-    const [active, setActive] = useState(types[0]);
+    // const [active, setActive] = useState(types[0]);
     return (
-        <>
-        <ButtonGroup>
-            {types.map(type => (
-                <Tab
-                    key={type}
-                    active={active === type}
-                    onClick={() => setActive(type)}
-                >
-                    {type}
-                </Tab>
-            ))}
-        </ButtonGroup>
-        <p />
-        {active == 'Lend' ? <Lend/> : <Borrow/>}
-        </>
+        <Tabs variant='soft-rounded' colorScheme='green'>
+            <TabList>
+                <Tab>Lend</Tab>
+                <Tab>Borrow</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <Lend />
+                </TabPanel>
+                <TabPanel>
+                    <Borrow />
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
+        // <>
+        // <ButtonGroup>
+        //     {types.map(type => (
+        //         <Tab
+        //             key={type}
+        //             active={active === type}
+        //             onClick={() => setActive(type)}
+        //         >
+        //             {type}
+        //         </Tab>
+        //     ))}
+        // </ButtonGroup>
+        // <p />
+        // {active == 'Lend' ? <Lend/> : <Borrow/>}
+        // </>
     );
 }
 export default TabGroup
