b68dff458128644b8e9935b11817caaa950e7cb1
Pochetes <robertomiguel2001@gmail.com>
2022-05-22 04:39:49 -0400

    implemented deposit and withdraw smart contract functions on Lend component

--- src/components/Lend.js
+++ src/components/Lend.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form';
+import React, { useEffect, useState, useCallback } from 'react'
+import { useForm, r} from 'react-hook-form';
+import * as Web3 from 'web3/dist/web3.min.js'
+import { constants } from 'ethers';
 import {
     FormControl,
     FormLabel,
@@ -8,28 +10,118 @@ import {
     Heading,
     Box
 } from '@chakra-ui/react';
-import { constants } from 'ethers';
+
+const ZEROMIN = 0;
 
 function Lend({ account, depPool, token0, token1}) {
-    const [token0Amt, setToken0Amt] = useState(0);
-    const [token1Amt, setToken1Amt] = useState(0);
     const [liquidityAmt, setLiquidityAmt] = useState(0);
-    const { register, handleSubmit } = useForm();
-    const { register: register2, handleSubmit: handleSubmit2 } = useForm();
+    const { register, handleSubmit, reset } = useForm({
+        defaultValues: {
+            token0Amt: '0',
+            token1Amt: '0',
+        }
+    });
+    const { register: register2, handleSubmit: handleSubmit2 } = useForm({
+        defaultValues: {
+            balance: '0', 
+        }
+    });
 
-    async function deposit({ token0, token1 }) {
-        // TODO
+    async function deposit({ token0Amt, token1Amt}) {
+        if (checkAllowance(account, token0) <= 0) {
+            await approve(token0, depPool._address)
+        }
+
+        if (checkAllowance(account, token1) <= 0) {
+            await approve(token1, depPool._address)
+        }
+
+        const addLiquidity = await depPool
+        .methods
+        .addLiquidity(
+            Web3.utils.toWei(token0Amt, "ether"),
+            Web3.utils.toWei(token1Amt, "ether"),
+            ZEROMIN,
+            ZEROMIN,
+            account
+        ).send({ from: account })
+        .then(res => {
+            alert("Liquidity has been Deposited.")
+            
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+
     }
 
     async function withdraw({ balance }) {
-        // TODO
+        await approveWithdraw(depPool, depPool._address)
+
+        const removeLiquidity = await depPool
+        .methods
+        .removeLiquidity(
+            Web3.utils.toWei(balance, "ether"),
+            ZEROMIN,
+            ZEROMIN,
+            account
+        )
+        .send({ from: account })
+        .then((res) => {
+            console.log(res)
+            return res
+        })
+        .catch(err => {
+            console.error(err)
+        })
     }
 
-    async function approve(_token) {
-        console.log("approve() >> " + _token.symbol);
-        const res = await _token.contract.methods.approve(depPool._address, constants.MaxUint256).send({ from: account});
+    async function approve(fromToken, toAddr) {
+        console.log(fromToken);
+        const res = await fromToken.contract.methods.approve(toAddr, constants.MaxUint256).send({ from: account });
         console.log("res >>");
         console.log(res);
+    }
+
+    async function approveWithdraw(depPool, depPoolAddr) {
+        console.log(depPool);
+        const res = await depPool.methods.approve(depPoolAddr, constants.MaxUint256.toString()).send({ from: account });
+        console.log("withdraw res >>");
+        console.log(res);
+    }
+
+    async function checkAllowance(account, token) {
+        console.log("checking allowance...")
+        if (token.symbol) console.log(token.symbol)
+        const allowedAmt = await token
+        .contract
+        .methods
+        .allowance(account, depPool._address)
+        .call()
+        .then(res => {
+            console.log(res)
+            return res
+        })
+        .catch(err => {
+            console.log("IM HERE")
+            console.error(err)
+        })
+    }
+
+    async function checkWithdrawAllowance(account, _depPool) {
+        const allowedAmt = await _depPool
+        .methods
+        .allowance(account, depPool._address)
+        .call()
+        .then(res => {
+            console.log(res)
+            return res
+        })
+        .catch(err => {
+            console.log("IM HERE")
+            console.error(err)
+        })
     }
 
 
@@ -54,9 +146,10 @@ function Lend({ account, depPool, token0, token1}) {
                         </FormLabel>
                         <Input
                             placeholder='amount'
+                            color={'#e2e8f0'}
                             id='token0'
-                            type='text'
-                            {...register('token0')}
+                            type='number'
+                            {...register('token0Amt')}
                         />
                         <FormLabel
                             color={'#e2e8f0'}
@@ -69,10 +162,14 @@ function Lend({ account, depPool, token0, token1}) {
                         </FormLabel>
                         <Input
                             placeholder='amount'
+                            color={'#e2e8f0'}
                             id='token1'
-                            type='text'
-                            {...register('token1')}
+                            type='number'
+                            {...register('token1Amt')}
                         />
+                        <Button type='submit' onClick={() => {
+                            reset()
+                        }}>Reset</Button>
                         <Button
                             my={5}
                             colorScheme='blue'
@@ -96,7 +193,8 @@ function Lend({ account, depPool, token0, token1}) {
                         </FormLabel>
                             <Input
                                 id='balance'
-                                type='text'
+                                color={'#e2e8f0'}
+                                type='number'
                                 {...register2('balance')}
                             />
                         <Button
